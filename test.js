e2a156cb444b1333c02ba0b9aa5531697862d7ce
kumailr7 <kumailrizvi70@gmail.com>
2024-09-21 10:50:14 +0530

    Adding secenrio to the test

--- test.js
+++ test.js
@@ -2,14 +2,14 @@ import http from 'k6/http';
 import { sleep } from 'k6';
 
 export const options = {
-  // A number specifying the number of VUs to run concurrently.
-  vus: 60,
-  // A string specifying the total duration of the test run.
-  duration: '1m',
-  thresholds: {
-    http_req_failed: ['rate<0.01'], // http errors should be less than 1%
-    http_req_duration: ['p(90)<500'], // 95 percent of response times must be below 500ms
-  },
+  // // A number specifying the number of VUs to run concurrently.
+  // vus: 60,
+  // // A string specifying the total duration of the test run.
+  // duration: '1m',
+  // thresholds: {
+  //   http_req_failed: ['rate<0.01'], // http errors should be less than 1%
+  //   http_req_duration: ['p(90)<500'], // 95 percent of response times must be below 500ms
+  // },
 
   scenarios: {
     example_scenario: { 
@@ -25,7 +25,7 @@ export const options = {
       iterations: 200,
       maxDuration: '10s',
     }
-  }
+  },
 
   // The following section contains configuration options for execution of this
   // test script in Grafana Cloud.
